216da7eefcd73757c81b6f34cb8599e96f3bbd56
Aron Nopanen <aron.nopanen@gmail.com>
2025-09-07 08:14:00 -0700

    Nah, use vite

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,6 @@
-import { assertEquals } from "@std/assert";
+import { expect, test } from 'vitest';
 import { add } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+test(function addTest() {
+  expect(add(2, 3)).to.equal(5);
 });
--- src/client/app.ts
+++ src/client/app.ts
@@ -1,3 +1,6 @@
 export function initializeApp() {
     console.log('Client application initialized');
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Initialized';
+    document.body.appendChild(h1);
 }
